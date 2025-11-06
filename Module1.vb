194d25298ce5d43dece32e9be6062968a2d94715
sccadmin <sccadmin@SCCNO504>
2025-11-06 14:19:07 +0900

    プロジェクト ファイルを追加します。

---
+++ Module1.vb
@@ -0,0 +1,10 @@
+﻿Imports MyLibrary
+Imports MyLibrary.Class1
+
+Module Module1
+    Sub Main()
+        Dim g As New Greeter()
+        g.SayHello("田中")
+        Console.ReadLine()
+    End Sub
+End Module
